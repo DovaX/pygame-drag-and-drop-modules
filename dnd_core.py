6af0e25b19932059cb7fc637a5f417deec60e597
Dominik Vach <vach.dominik@gmail.com>
2020-10-18 23:59:35 +0200

    Link between modules

--- dnd_core.py
+++ dnd_core.py
@@ -2,3 +2,12 @@ class Module:
     def __init__(self,coordinates):
         self.coordinates=coordinates
     
+    
+    
+class Link:
+    def __init__(self,input_module,output_module):
+        self.input_module=input_module
+        self.output_module=output_module
+    
+    
+    
